4c4c42d0c34b24b289df189da9e6074320118bed
Chris Walsh <attrition.h@gmail.com>
2016-01-18 21:46:24 -0500

    Camera can follow different ships on TAB
    Fighter turn speed lowered

--- Assets/Resources/Scripts/Game.cs
+++ Assets/Resources/Scripts/Game.cs
@@ -1,21 +1,40 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class Game : MonoBehaviour
 {
+    public List<Ship> Ships;
+    public int CurrentShip = -1;
+    public ShipOrthoFollowCamera orthoFollowCam;
+
     // Use this for initialization
     void Start()
     {
-        for (int z = -1000; z <= 1000; z += 100)
-            Debug.DrawLine(new Vector3(-1000, -10, z), new Vector3(1000, -10, z), Color.blue, 300f);
+        for (int z = -10000; z <= 10000; z += 100)
+            Debug.DrawLine(new Vector3(-10000, -10, z), new Vector3(10000, -10, z), Color.blue, 300f);
 
-        for (int x = -1000; x <= 1000; x += 100)
-            Debug.DrawLine(new Vector3(x, -10, -1000), new Vector3(x, -10, 1000), Color.blue, 300f);
+        for (int x = -10000; x <= 10000; x += 100)
+            Debug.DrawLine(new Vector3(x, -10, -10000), new Vector3(x, -10, 10000), Color.blue, 300f);
+
+        Ships = new List<Ship>(this.gameObject.GetComponentsInChildren<Ship>());
+        if (Ships.Count > 0)
+            CurrentShip = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (Ships.Count > 0)
+            {
+                Ships[CurrentShip].playerControlled = false;
+                CurrentShip++;
+                if (CurrentShip >= Ships.Count)
+                    CurrentShip = 0;
+                Ships[CurrentShip].playerControlled = true;
+                orthoFollowCam.Target = Ships[CurrentShip].transform;
+            }
+        }
     }
 }
--- Assets/Resources/Scripts/ShipOrthoFollowCamera.cs
+++ Assets/Resources/Scripts/ShipOrthoFollowCamera.cs
@@ -1,30 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ShipFollowCamera : MonoBehaviour
+public class ShipOrthoFollowCamera : MonoBehaviour
 {
     public Camera ActiveCamera = null;
-    public Transform Ship = null;
+    public Transform Target = null;
     public float FollowHeight = 200f;
     public float OrthoSize = 150f;
-
-    private Ship following;
 
     // Use this for initialization
     void Start()
     {
-        following = Ship.gameObject.GetComponent<Ship>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (ActiveCamera == null || Ship == null)
+        if (ActiveCamera == null || Target == null)
             return;
 
-        ActiveCamera.transform.position = Ship.transform.position;
-        ActiveCamera.transform.position += new Vector3(0f, FollowHeight);        
-        ActiveCamera.orthographicSize = following.DesiredCameraOrthoSize;
+        ActiveCamera.transform.position = Target.transform.position;
+        ActiveCamera.transform.position += new Vector3(0f, FollowHeight);
+        ActiveCamera.orthographicSize = Target.gameObject.GetComponent<Ship>().DesiredCameraOrthoSize;
     }
 
     private float RemapRange(float remapValue, float oldMin, float oldMax, float newMin, float newMax)
